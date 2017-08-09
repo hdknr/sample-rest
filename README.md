ec3e3c57059457fb867ab9b4bccea7f0503bf249
hdknr <gmail@hdknr.com>
2017-08-09 16:31:50 +0900

    blogs application

--- README.md
+++ README.md
@@ -4,5 +4,19 @@
 ~~~bash
 $ pip install -r requirements/pypi.txt
 $ django-admin startproject app web
+$ python manage.py startapp blogs
 ~~~
 
+~~~bash
+$ python manage.py makemigrations
+$ python manage.py migrate
+~~~
+
+~~~bash
+$ python manage.py createsuperuser
+Username (leave blank to use 'vagrant'):
+Email address: admin@vagrant.local
+Password:
+Password (again):
+Superuser created successfully.
+~~~
---
+++ web/blogs/models.py
@@ -0,0 +1,32 @@
+# coding: utf-8
+from django.db import models
+from django.utils.translation import ugettext_lazy as _
+from django.contrib.auth.models import User
+
+
+class Status(models.Model):
+    code = models.CharField(max_length=20, unique=True)
+    name = models.CharField(max_length=20)
+
+    class Meta:
+        verbose_name = _('Article Status')
+        verbose_name_plural = _('Article Status')
+
+    def __unicode__(self):
+        return self.name
+
+
+class Article(models.Model):
+    status = models.ForeignKey(Status)
+    author = models.ForeignKey(User)
+    title = models.CharField(max_length=128)
+    body = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        verbose_name = _('Article')
+        verbose_name_plural = _('Articles')
+
+    def __unicode__(self):
+        return self.title
