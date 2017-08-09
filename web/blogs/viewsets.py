9be20e19c909ab3ffe257642a5038c752f4ce5e2
hdknr <gmail@hdknr.com>
2017-08-09 17:14:05 +0900

    Model Permission

--- web/blogs/viewsets.py
+++ web/blogs/viewsets.py
@@ -1,6 +1,9 @@
 # coding: utf-8
 import django_filters
 from rest_framework import viewsets, filters
+from rest_framework.permissions import (
+    DjangoModelPermissionsOrAnonReadOnly as BasePerm
+)
 
 from django.contrib.auth.models import User
 from . import models, serializers
@@ -9,13 +12,16 @@ from . import models, serializers
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = serializers.UserSerializer
+    permission_classes = [BasePerm, ]
 
 
 class StatusViewSet(viewsets.ModelViewSet):
     queryset = models.Status.objects.all()
     serializer_class = serializers.StatusSerializer
+    permission_classes = [BasePerm, ]
 
 
 class ArticleViewSet(viewsets.ModelViewSet):
     queryset = models.Article.objects.all()
     serializer_class = serializers.ArticleSerializer
+    permission_classes = [BasePerm, ]
